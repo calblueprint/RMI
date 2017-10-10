7c11f00fc34a26e66e09321b38ab852a713ca24d
arpanpal <arpan.pal1234@gmail.com>
2017-10-09 19:23:40 -0700

    created models from issue 8, did not implement tests but tested in console (#14)
    * created models from issue 8, did not implement tests but tested in console
    
    * style fixes
    
    * style fixes

---
+++ app/models/building_operator.rb
@@ -0,0 +1,11 @@
+class BuildingOperator < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, :last_name, presence: true
+  # email validation with regex
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates :phone, :presence => true, :numericality => true, :length => { :minimum => 10, :maximum => 15}
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :building_operators
+  devise_for :asset_managers
+  devise_for :rmi_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   namespace :api, defaults: { format: :json } do
