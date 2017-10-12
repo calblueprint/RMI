871e0a097e6abb3247faf237a7398e479942c1d6
arpanpal <arpan.pal1234@gmail.com>
2017-10-12 00:19:57 -0700

    Revert "Added necessary fields to each and tested, didn't write tests"
    This reverts commit 72d7e068913824666173b8a4f0f742ccfb8453a5.
    
    "reverting answer changes, need to submit pull request first"

--- app/models/building_operator.rb
+++ app/models/building_operator.rb
@@ -4,8 +4,6 @@ class BuildingOperator < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :answers
-
   validates :first_name, :last_name, presence: true
   # email validation with regex
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
