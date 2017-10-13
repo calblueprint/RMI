1da8ff501445f3c6bde963a68bc30030f54c4d47
arpanpal <arpan.pal1234@gmail.com>
2017-10-12 22:51:42 -0700

    had to update schema to match current setup (no STI for users)

--- db/schema.rb
+++ db/schema.rb
@@ -157,28 +157,7 @@ ActiveRecord::Schema.define(version: 20171012070834) do
     t.index ["email"], name: "index_rmi_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_rmi_users_on_reset_password_token", unique: true
   end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.string "phone_number"
-    t.integer "type"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
+  
   add_foreign_key "answers", "building_operators"
   add_foreign_key "answers", "buildings"
   add_foreign_key "answers", "questions"
