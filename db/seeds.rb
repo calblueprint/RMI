b63a41f8f9943b7117aa31a97d86cde20bd3ddc0
Ethan Lee <hello@ethanl.ee>
2017-10-13 02:29:16 -0700

    Finish question generation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,46 +1,145 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
+## BuildingTypes
 
 large_cube_retail = BuildingType.create({ name: 'Large Cube Retail' })
 rockclimbing_eggyolk_inc = BuildingType.create({ name: 'REI' })
 
-free_questions: [
-  'Describe the kind of cubes you enjoy most.',
-  'How many cubes do you currently have in your building?',
-  'Please provide your three desired cube colors.'
+# Questions
+# -----
+# FORMAT
+# [<text>, <type>, <array of options>, <array of dependents (or nils)>]
+
+no_dependents = Array.new(3, nil);
+safe_maximum = 2 ** 53 - 1
+
+large_cube_retail_questions = [
+  ['Describe the kind of cubes you enjoy most.', :free]
+  ['How many cubes do you currently have in your building?', :free]
+  ['Please provide your three desired cube colors.', :free],
+  ['Who manufactures your cubes?', :dropdown, 
+    ['Apple', 'Google', 'Samsung'], no_dependents],
+  ['During which time slot are you available for a cube inspection?', :dropdown,
+    ['12:00-1:00 PM', '1:00-2:00 PM', '2:00-3:00 PM'], no_dependents],
+  ['What model are your cubes?', :dropdown,
+    ['Model T', 'Corolla', 'Leaf'], no_dependents],
+  ['Please specify the amount of faces your cube has.', :range,
+    [[0, 6]], no_dependents],
+  ['How many corners does your cube have?', :range,
+    [[0, 8]], no_dependents],
+  ['Please evaluate your cube on a scale of 1-10: 1 = Strongly Disagree, 10 = Strongly Agree', :range,
+    [[0, 10]], no_dependents],
+  [
+    'Do you love your cube?', :dropdown,
+    ['Yes.', 'Not really'],
+    [[
+      'How many times a day do you tell your cube that you love it?', :range,
+      [[0, 3], [4, 7], [8, 10]],
+      [
+        [ 'List some ways you can spend more time with your cube.', :free ],
+        [ 'What has been your favorite reaction to affection shown towards your cube?', :free ],
+        [ 'Does your cube enjoy the attention?', :dropdown, ['Yes', 'No'], [nil, nil] ]
+      ]
+    ], nil]
+  ]
 ]
 
-dropdown_questions: [
-  ['Who manufactures your cubes?', 'Apple', 'Google', 'Samsung'],
-  ['During which time slot are you available for a cube inspection?', '12:00-1:00 PM', '1:00-2:00 PM', '2:00-3:00 PM']
-  ['What model are your cubes?', 'Model T', ''
+rockclimbing_eggyolk_inc_questions = [
+  ['Please provide the name of the rockclimbing gym of your eggyolk\'s choice.', :free],
+  [
+    'How many years has your eggyolk been rockclimbing?', :range,
+    [[0, 2], [3, 5], [6, safe_maximum]],
+    [
+      ['Has your eggyolk taken lessons?', :free],
+      [
+        'How does your egg prepare for rockclimbing?', :dropdown,
+        ['Hardboiled', 'Scrambled', 'Overeasy'], no_dependents
+      ],
+      [
+        'How many hours a day does your eggyolk spend rockclimbing?', :range,
+        [[0, 4], [5, 8], [0, 24]],
+        [
+          ['Describe your eggyolk\'s work-life balance.', :free],
+          ['Is your eggyolk often exhausted?', :dropdown, ['Yes', 'No'], no_dependents],
+          nil
+        ]
+      ]
+    ]
+  ],
+  [
+    'Does your eggyolk have any allergies?', :dropdown,
+    ['Yes', 'No'],
+    [['List any foods your eggyolk may be allergic to.', :free], nil] 
+  ],
+  ['What is your eggyolk\'s budget on rockclimbing?', :range,
+    [[0, 100], [101, 500], [501, safe_maximum]],
+    [nil, nil, ['Is your eggyolk employed?', :dropdown, ['Yes', 'No'], no_dependents]]
+  ],
+  ['Briefly describe how your eggyolk discovered rockclimbing.', :free],
+  ['Explain why your eggyolk enjoys rockclimbing, if applicable.', :free],
+  ['What kind of shoes does your eggyolk use when rockclimbing?', :dropdown,
+    ['Laces', 'Velcro', 'Slipper'], no_dependents
+  ]
+  ['Which kind of rockclimbing does your eggyolk engage in?', :dropdown,
+    ['Bouldering', 'Top Rope Climbing', 'Mountaineering'], no_dependents
+  ],
+  ['How many shoes does your eggyolk own?', :range, [[0, 10]], no_dependents],
+  ['What size shoe is your eggyolk?', :range, [[0, 20]], no_dependents]
 ]
 
+def generate_question(question, building_type, parent_option=nil)
+  if question.nil?
+    return
+  end
+  q_text = question[0]
+  q_type = question[1]
 
-questions = Question.create([{
-  question_type: :free,
-  status: :published,
-  text: ,
-  building_type: large_cube_retail
-}, {
-  question_type: :free,
-  status: :published,
-  text: ,
-  building_type: large_cube_retail
-}, {
-  question_type: :free,
-  status: :published,
-  text: 
-  building_type: large_cube_retail
-}, {
-  question_type: :dropdown,
-  status: :published,
-  text: 
-}]
+  if q_type.equal? :free
+    Question.create({
+      question_type: :free,
+      status: :published,
+      text: q_text,
+      building_type: building_type,
+      parent_option: parent_option
+      })
+    return
+  end
+
+  q_options = question[2]
+  q_dependents = question[3]
+  if q_type.equal? :dropdown
+    q = Question.create({
+      question_type: :dropdown,
+      status: :published,
+      text: q_text,
+      building_type: building_type,
+      parent_option: parent_option
+      })
+    q_options.map do |option|
+      DropdownOption.create({
+        text: option,
+        question: q
+        })
+    end
+  end
+  if q_type.equal? :range
+    q = Question.create({
+      question_type: :range,
+      status: :published,
+      text: q_text,
+      building_type: building_type,
+      parent_option: parent_option
+      })
+    q_options.map do |option|
+      RangeOption.create({
+        min: option[0],
+        max: option[1],
+        question: q
+        })
+    end
+  end
+
+  Array(0...q_options.length).each do |i|
+    generate_question(q_dependents[i], building_type, q_options[i])
+  end
+end
+
 
