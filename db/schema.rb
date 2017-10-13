c7c6aa50471facdb9259b260687ec2fb4cb4a72c
arpanpal <arpan.pal1234@gmail.com>
2017-10-12 22:42:01 -0700

    updating has_many to has_one

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010181113) do
+ActiveRecord::Schema.define(version: 20171012070834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,9 @@ ActiveRecord::Schema.define(version: 20171010181113) do
     t.bigint "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "building_operator_id"
     t.index ["building_id"], name: "index_answers_on_building_id"
+    t.index ["building_operator_id"], name: "index_answers_on_building_operator_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
@@ -156,6 +158,28 @@ ActiveRecord::Schema.define(version: 20171010181113) do
     t.index ["reset_password_token"], name: "index_rmi_users_on_reset_password_token", unique: true
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.integer "type"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "answers", "building_operators"
   add_foreign_key "answers", "buildings"
   add_foreign_key "answers", "questions"
   add_foreign_key "buildings", "portfolios"
