4d888006957f0a3d80657148b68ffe373cab2648
Ethan Lee <hello@ethanl.ee>
2017-10-15 13:38:40 -0700

    Update for buildings, portfolios, and locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,131 +14,126 @@ eggyolk_information = Category.create!({ name: 'Personal Information' })
 # Questions
 # -----
 # FORMAT
-# [<text>, <type>, <array of options>, <array of dependents (or nils)>]
+# [<text>, <category>, <type>, <array of options>, <array of dependents (or nils)>]
 
 no_dependents = Array.new(3, nil);
 safe_maximum = 2 ** 31 - 1
 
 large_cube_retail_questions = [
-  ['Describe the kind of cubes you enjoy most.', :free, cube_facts],
-  ['How many cubes do you currently have in your building?', :free, cube_facts],
-  ['Please provide your three desired cube colors.', :free, cube_facts],
-  ['Who manufactures your cubes?', :dropdown, 
-    ['Apple', 'Google', 'Samsung'], no_dependents, cube_facts],
-  ['During which time slot are you available for a cube inspection?', :dropdown,
-    ['12:00-1:00 PM', '1:00-2:00 PM', '2:00-3:00 PM'], no_dependents, cube_facts],
-  ['What model are your cubes?', :dropdown,
-    ['Model T', 'Corolla', 'Leaf'], no_dependents, cube_facts],
-  ['Please specify the amount of faces your cube has.', :range,
-    [[0, 6]], no_dependents, cube_facts],
-  ['How many corners does your cube have?', :range,
-    [[0, 8]], no_dependents, cube_facts],
-  ['Please evaluate your cube on a scale of 1-10: 1 = Strongly Disagree, 10 = Strongly Agree', :range,
-    [[0, 10]], no_dependents, cube_care],
+  ['Describe the kind of cubes you enjoy most.', cube_facts, :free],
+  ['How many cubes do you currently have in your building?', cube_facts, :free],
+  ['Please provide your three desired cube colors.', cube_facts, :free],
+  ['Who manufactures your cubes?', cube_facts, :dropdown, 
+    ['Apple', 'Google', 'Samsung'], no_dependents],
+  ['During which time slot are you available for a cube inspection?', cube_facts, :dropdown,
+    ['12:00-1:00 PM', '1:00-2:00 PM', '2:00-3:00 PM'], no_dependents],
+  ['What model are your cubes?', cube_facts, :dropdown,
+    ['Model T', 'Corolla', 'Leaf'], no_dependents],
+  ['Please specify the amount of faces your cube has.', cube_facts, :range,
+    [[0, 6]], no_dependents],
+  ['How many corners does your cube have?', cube_facts, :range,
+    [[0, 8]], no_dependents],
+  ['Please evaluate your cube on a scale of 1-10: 1 = Strongly Disagree, 10 = Strongly Agree', cube_care, :range,
+    [[0, 10]], no_dependents],
   [
-    'Do you love your cube?', :dropdown,
+    'Do you love your cube?', cube_care, :dropdown,
     ['Yes.', 'Not really'],
     [[
-      'How many times a day do you tell your cube that you love it?', :range,
+      'How many times a day do you tell your cube that you love it?', cube_care, :range,
       [[0, 3], [4, 7], [8, 10]],
       [
-        [ 'List some ways you can spend more time with your cube.', :free, cube_care ],
-        [ 'What has been your favorite reaction to affection shown towards your cube?', :free, cube_care ],
-        [ 'Does your cube enjoy the attention?', :dropdown, ['Yes', 'No'], [nil, nil], cube_care ]
-      ], cube_care
-    ], nil],
-    cube_care
+        [ 'List some ways you can spend more time with your cube.', cube_care, :free ],
+        [ 'What has been your favorite reaction to affection shown towards your cube?', cube_care, :free ],
+        [ 'Does your cube enjoy the attention?', cube_care, :dropdown, ['Yes', 'No'], [nil, nil] ]
+      ]
+    ], nil]
   ]
 ]
 
 rockclimbing_eggyolk_inc_questions = [
-  ['Please provide the name of the rockclimbing gym of your eggyolk\'s choice.', :free],
+  ['Please provide the name of the rockclimbing gym of your eggyolk\'s choice.', rockclimbing_skill, :free],
   [
-    'How many years has your eggyolk been rockclimbing?', :range,
+    'How many years has your eggyolk been rockclimbing?', rockclimbing_skill, :range,
     [[0, 2], [3, 5], [6, safe_maximum]],
     [
-      ['Has your eggyolk taken lessons?', :free],
+      ['Has your eggyolk taken lessons?', rockclimbing_skill, :free],
       [
-        'How does your egg prepare for rockclimbing?', :dropdown,
+        'How does your egg prepare for rockclimbing?', rockclimbing_skill, :dropdown,
         ['Hardboiled', 'Scrambled', 'Overeasy'], no_dependents
       ],
       [
-        'How many hours a day does your eggyolk spend rockclimbing?', :range,
+        'How many hours a day does your eggyolk spend rockclimbing?', rockclimbing_skill, :range,
         [[0, 4], [5, 8], [0, 24]],
         [
-          ['Describe your eggyolk\'s work-life balance.', :free],
-          ['Is your eggyolk often exhausted?', :dropdown, ['Yes', 'No'], no_dependents],
+          ['Describe your eggyolk\'s work-life balance.', rockclimbing_skill, :free],
+          ['Is your eggyolk often exhausted?', rockclimbing_skill, :dropdown, ['Yes', 'No'], no_dependents],
           nil
         ]
       ]
     ]
   ],
   [
-    'Does your eggyolk have any allergies?', :dropdown,
+    'Does your eggyolk have any allergies?', eggyolk_information, :dropdown,
     ['Yes', 'No'],
-    [['List any foods your eggyolk may be allergic to.', :free], nil] 
+    [['List any foods your eggyolk may be allergic to.', eggyolk_information, :free], nil] 
   ],
-  ['What is your eggyolk\'s budget on rockclimbing?', :range,
+  ['What is your eggyolk\'s budget on rockclimbing?', eggyolk_information, :range,
     [[0, 100], [101, 500], [501, safe_maximum]],
-    [nil, nil, ['Is your eggyolk employed?', :dropdown, ['Yes', 'No'], no_dependents]]
+    [nil, nil, ['Is your eggyolk employed?', eggyolk_information, :dropdown, ['Yes', 'No'], no_dependents]]
   ],
-  ['Briefly describe how your eggyolk discovered rockclimbing.', :free],
-  ['Explain why your eggyolk enjoys rockclimbing, if applicable.', :free],
-  ['What kind of shoes does your eggyolk use when rockclimbing?', :dropdown,
+  ['Briefly describe how your eggyolk discovered rockclimbing.', eggyolk_information, :free],
+  ['Explain why your eggyolk enjoys rockclimbing, if applicable.', eggyolk_information, :free],
+  ['What kind of shoes does your eggyolk use when rockclimbing?', eggyolk_information, :dropdown,
     ['Laces', 'Velcro', 'Slipper'], no_dependents
   ],
-  ['Which kind of rockclimbing does your eggyolk engage in?', :dropdown,
+  ['Which kind of rockclimbing does your eggyolk engage in?', eggyolk_information, :dropdown,
     ['Bouldering', 'Top Rope Climbing', 'Mountaineering'], no_dependents
   ],
-  ['How many shoes does your eggyolk own?', :range, [[0, 10]], no_dependents],
-  ['What size shoe is your eggyolk?', :range, [[0, 20]], no_dependents]
+  ['How many shoes does your eggyolk own?', eggyolk_information, :range, [[0, 10]], no_dependents],
+  ['What size shoe is your eggyolk?', eggyolk_information, :range, [[0, 20]], no_dependents]
 ]
 
 def generate_question(question, building_type, parent_option=nil)
   if question.nil?
     return
   end
+
   q_text = question[0]
-  q_type = question[1]
+  q_category = question[1]
+  q_type = question[2]
+  q_options = question[3]
+  q_dependents = question[4]
+
+  q = Question.create!({
+    question_type: q_type,
+    status: :published,
+    text: q_text,
+    building_type: building_type,
+    category: q_category
+    })
+
+  unless parent_option.nil?
+    q.parent_option = parent_option
+    q.save!
+  end
+
+  print("creating\n", q_text, " ", parent_option, " ", q.parent_option_id, "\n--------------\n")
 
   if q_type.equal? :free
-    Question.create!({
-      question_type: :free,
-      status: :published,
-      text: q_text,
-      building_type: building_type,
-      parent_option: parent_option
-      })
     return
   end
 
-  q_options = question[2]
-  q_dependents = question[3]
   if q_type.equal? :dropdown
-    q = Question.create!({
-      question_type: :dropdown,
-      status: :published,
-      text: q_text,
-      building_type: building_type,
-      parent_option: parent_option
-      })
-    q_options = q_options.map do |option|
-      DropdownOption.create({
+    q_options_saved = q_options.map do |option|
+      DropdownOption.create!({
         text: option,
         question: q
         })
     end
   end
   if q_type.equal? :range
-    q = Question.create!({
-      question_type: :range,
-      status: :published, 
-      text: q_text,
-      building_type: building_type,
-      parent_option: parent_option
-      })
-    q_options = q_options.map do |option|
-      RangeOption.create({
+    q_options_saved = q_options.map do |option|
+      RangeOption.create!({
         min: option[0],
         max: option[1],
         question: q
@@ -146,37 +141,13 @@ def generate_question(question, building_type, parent_option=nil)
     end
   end
 
-  Array(0...q_options.length).each do |i|
-    generate_question(q_dependents[i], building_type, q_options[i])
+  Array(0...q_options_saved.length).each do |i|
+    generate_question(q_dependents[i], building_type, q_options_saved[i])
   end
 end
 
 large_cube_retail_questions.each { |q| generate_question(q, large_cube_retail) }
 rockclimbing_eggyolk_inc_questions.each { |q| generate_question(q, rockclimbing_eggyolk_inc) }
-
-# Locations
-# -----
-# FORMAT
-# [<name>, <address>, <city>, <state>, <zip>]
-addresses = [
-  ['Unit 1', '2650 Durant Ave', 'Berkeley', :California, 94704],
-  ['Northside Cafe', '1878 Euclid Ave', 'Berkeley', :California, 94709],
-  ['MLK Student Union', '2560 Bancroft Way', 'Berkeley', :California, 94704],
-  ['SMCHS', '22062 Antonio Pkwy', 'Rancho Santa Margarita', :California, 92688],
-  ['Monta Vista HS', '21840 McClellan Rd', 'Cupertino', :California, 95014],
-  ['Archbishop Mitty', '5000 Mitty Way', 'San Jose', :California, 95129],
-  ['The House', '2495 Bancroft Way', 'Berkeley', :California, 94720],
-  ['Googleplex', '1600 Amphitheatre Pkwy', 'Mountain View', :California, 94043],
-  ['Apple Park', '1 Apple Park Way', 'Cupertino', :California, 95014],
-  ['Facebook HQ', '1 Hacker Way', 'Menlo Park', :California, 94025],
-  ['Mozilla HQ', '2 Harrison St', 'San Francisco', :California, 94105],
-  ['Horizons HQ', '450 9th St', 'San Francisco', :California, 94103],
-  ['Little Gem Belgian Waffles', '2468 Telegraph Ave A', 'Berkeley', :California, 94704],
-  ['Gypsy\'s Trattoria Italiana', '2519 Durant Ave', 'Berkeley', :California, 94704],
-  ['People\'s Park', '2556 Haste St', 'Berkeley', :California, 94704],
-]
-
-# Buildings
 
 # RMI Users
 people = [
@@ -231,23 +202,89 @@ people = [
 ]
 
 users_per_type = people.length / 3
-rmi_users = people[0..1 * users_per_type].map do |p|
-  RmiUser.create({
+def person(p)
+  {
     first_name: p[0],
     last_name: p[1],
     email: p[2],
     phone: p[3],
     password: p[4],
     password_confirmation: p[4]
-    })
+    }
 end
 
-# people[1 * users_per_type..2 * users_per_type].map{ |p| AssetManager.create({ first_name: }) }
-# people[2 * users_per_type..3 * users_per_type].map{ |p| }
-
+rmi_users = people[0..1 * users_per_type].map do |p|
+  RmiUser.create(person(p))
+end
 
 # Asset Manager Users
+asset_managers = people[1 * users_per_type..2 * users_per_type].map do |p|
+  AssetManager.create(person(p))
+end
 
 # Building Operator Users
+building_operators = people[2 * users_per_type..3 * users_per_type].map do |p|
+  BuildingOperator.create(person(p))
+end
 
+# Locations
+# -----
+# FORMAT
+# [<name>, <address>, <city>, <state>, <zip>]
+addresses = [
+  ['Unit 1', '2650 Durant Ave', 'Berkeley', :California, 94704],
+  ['Northside Cafe', '1878 Euclid Ave', 'Berkeley', :California, 94709],
+  ['MLK Student Union', '2560 Bancroft Way', 'Berkeley', :California, 94704],
+  ['SMCHS', '22062 Antonio Pkwy', 'Rancho Santa Margarita', :California, 92688],
+  ['Monta Vista HS', '21840 McClellan Rd', 'Cupertino', :California, 95014],
+  ['Archbishop Mitty', '5000 Mitty Way', 'San Jose', :California, 95129],
+  ['The House', '2495 Bancroft Way', 'Berkeley', :California, 94720],
+  ['Googleplex', '1600 Amphitheatre Pkwy', 'Mountain View', :California, 94043],
+  ['Apple Park', '1 Apple Park Way', 'Cupertino', :California, 95014],
+  ['Facebook HQ', '1 Hacker Way', 'Menlo Park', :California, 94025],
+  ['Mozilla HQ', '2 Harrison St', 'San Francisco', :California, 94105],
+  ['Horizons HQ', '450 9th St', 'San Francisco', :California, 94103],
+  ['Little Gem Belgian Waffles', '2468 Telegraph Ave A', 'Berkeley', :California, 94704],
+  ['Gypsy\'s Trattoria Italiana', '2519 Durant Ave', 'Berkeley', :California, 94704],
+  ['People\'s Park', '2556 Haste St', 'Berkeley', :California, 94704],
+]
 
+def location(l)
+  {
+    address: l[1],
+    state: l[3],
+    zip: l[4]
+    }
+end
+
+# Portfolios
+portfolios = asset_managers.map do |a|
+  Portfolio.create!({ name: a.first_name + "'s Portfolio", asset_manager: a })
+end
+
+# Buildings
+Array(0..portfolios.length).each do |i|
+  portfolio = portfolios[i]
+  asset_manager = portfolio.asset_manager
+
+  Array(0..addresses.length).each do |j|
+    building_location = addresses[j];
+    building = Building.new({
+      portfolio: portfolio,
+      contact_email: asset_manager.email
+      })
+
+    if i % 2 == 0
+      building.building_type = large_cube_retail
+    else
+      building.building_type = rockclimbing_eggyolk_inc
+    end
+
+    building.name = building_location[0]
+    building.save!
+
+    location = Location.new(location(building_location[j]))
+    location.building = building
+    location.save!
+  end
+end
