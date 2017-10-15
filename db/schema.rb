6c5e29d4831eb2d2684fdf702bf52b91216d756d
Ethan Lee <hello@ethanl.ee>
2017-10-15 15:32:15 -0700

    Add join table for building <-> building operator relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171015215749) do
+ActiveRecord::Schema.define(version: 20171015221125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,11 @@ ActiveRecord::Schema.define(version: 20171015215749) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_asset_managers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_asset_managers_on_reset_password_token", unique: true
+  end
+
+  create_table "building_assignments", id: false, force: :cascade do |t|
+    t.bigint "building_id", null: false
+    t.bigint "building_operator_id", null: false
   end
 
   create_table "building_operators", force: :cascade do |t|
