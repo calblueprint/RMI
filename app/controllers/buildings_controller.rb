a6805917bb66e85cd2d7a7fe948d00a52c617ee6
arpanpal <arpan.pal1234@gmail.com>
2017-10-15 14:20:01 -0700

    moved json functions out of buildings controller

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,17 +1,4 @@
 class BuildingsController < ApplicationController
-  def create
-    # should always create buildings with a portfolio
-    @portfolio = Portfolio.find(params[:portfolio_id])
-    @building = @portfolio.buildings.create(building_params)
-  end
-
-  def update
-    # this is for when we set up the request to send a speific portfolio
-    # @portfolio = Portfolio.find(params[:portfolio_id])
-    # @building = @portfolio.buildings.find(params[:id])
-    @building = Building.find(params[:id])
-    @building.update(building_params)
-  end
 
   def show
     # this is for when we set up the request to send a specific portfolio
@@ -21,7 +8,5 @@ class BuildingsController < ApplicationController
     render 'show'
   end
 
-  def building_params
-    params.require(:building).permit(:name, :contact_email, :portfolio_id)
-  end
+
 end
