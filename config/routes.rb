ffef99d0f8379d12576e903d0af79556c29d49fb
Kevin Li <kevintli98@gmail.com>
2017-10-17 10:34:31 -0700

    Create portfolio HTML controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   resources :buildings, only: [:show]
+  resources :portfolios, only: [:show]
+
   namespace :api, defaults: { format: :json } do
     resources :buildings, only: %i[index create update]
   end
