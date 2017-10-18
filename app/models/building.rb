6df07131e795431035e0c63ff575cde615f80010
arpanpal <arpan.pal1234@gmail.com>
2017-10-17 21:21:45 -0700

    annotated

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: buildings
+#
+#  id               :integer          not null, primary key
+#  name             :string
+#  portfolio_id     :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  building_type_id :integer
+#  address          :string
+#  city             :string
+#  state            :integer
+#  zip              :integer
+#
+
 class Building < ApplicationRecord
   belongs_to :portfolio
   belongs_to :building_type
