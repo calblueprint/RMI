b9f958392f605248a4d0a2951d33258c58b7e5ea
Kevin Li <kevintli98@gmail.com>
2017-10-17 21:29:18 -0700

    Create answers API controller

---
+++ app/controllers/api/answers_controller.rb
@@ -0,0 +1,32 @@
+class Api::AnswersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    answer = Answer.new(answer_params)
+    if answer.save
+      render_json_message(:ok, message: 'New answer created', data: answer)
+    else
+      render_json_message(:forbidden, data: answer, errors: answer.errors.full_messages)
+    end
+  end
+
+  def update
+    answer = Answer.find(params[:id])
+    if answer.update(answer_params)
+      render_json_message(:ok, message: 'Answer successfully updated', data: answer)
+    else
+      render_json_message(:forbidden, data: answer, errors: answer.errors.full_messages)
+    end
+  end
+
+  private
+  def answer_params
+    params.require(:answer)
+        .permit(
+            :text,
+            :building,
+            :question,
+            :building_operator
+        )
+  end
+end
