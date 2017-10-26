ab833894060e03e0d500cab1718348210a4d91a8
arpanpal <arpan.pal1234@gmail.com>
2017-10-26 14:48:36 -0700

    rmi user view

--- app/controllers/rmi_users_controller.rb
+++ app/controllers/rmi_users_controller.rb
@@ -2,7 +2,7 @@ class RmiUsersController < ApplicationController
   def show
     @rmi_user = RmiUser.find(params[:id])
     if rmi_user_signed_in? && current_rmi_user.id == @rmi_user.id
-      @portfolios = Portfolios.all
+      @portfolios = Portfolio.all
     else
       @message = 'The RMI User is not currently logged in.'
     end
