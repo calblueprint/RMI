ac41ccff35e4818c19c7363eb5b7bcbf3babb89b
Justin Mi <justin.mi@berkeley.edu>
2017-10-29 11:43:34 -0700

    style changes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,14 +6,14 @@ class PortfoliosController < ApplicationController
   ##
   # Exports a CSV file for each Building Type in the portfolio.
   # Each CSV header contains the parameters of each question in the portfolio.
-  # Each row in a Building Type CSV file contains all the answers for a
-  # particular building of that Building Type.
+  # Each row in a CSV file contains all the answers for a particular building
+  # of that Building Type.
   #
   # References:
-  # https://github.com/rubyzip/rubyzip
   # http://www.rubydoc.info/github/rubyzip/rubyzip/master/toplevel/
   # http://ruby-doc.org/stdlib-2.0.0/libdoc/tempfile/rdoc/Tempfile.html
   # http://thinkingeek.com/2013/11/15/create-temporary-zip-file-send-response-rails/
+  # TODO: modularize everything. Make CSV file temporary too. 
   #
   def download
     portfolio = Portfolio.find(params[:id])
@@ -28,10 +28,10 @@ class PortfoliosController < ApplicationController
       Zip::File.open(temp_file.path, Zip::File::CREATE) do |zip|
         # for building_type in building_types, create csv for that building type
         # if the query result is not empty
-        BuildingType.find_each do |type|
-          buildings = portfolio.buildings.where(building_type: type).all
+        BuildingType.find_each do |building_type|
+          buildings = portfolio.buildings.where(building_type: building_type).all
           unless buildings.blank?
-            csv_name = "#{filename}_#{type.name}".delete(' ')
+            csv_name = "#{filename}_#{building_type.name}".delete(' ')
             # populate CSV
             CSV.open(csv_name, 'wb') do |csv|
               # add column headers to CSV
@@ -40,8 +40,6 @@ class PortfoliosController < ApplicationController
               buildings.each do |building|
                 csv << Building.column_names.map { |attr| building.send(attr) }
               end
-              puts csv_name
-              puts temp_file.path
               zip.add("#{csv_name}.csv", csv.path)
             end
           end
