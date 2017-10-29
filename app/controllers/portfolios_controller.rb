f61b5cb16fff837d9f566940a95572730bd758a8
Justin Mi <justin.mi@berkeley.edu>
2017-10-29 16:44:42 -0700

    modularized the export code, moved temporary file storage to tmp/zip

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,47 +13,67 @@ class PortfoliosController < ApplicationController
   # http://www.rubydoc.info/github/rubyzip/rubyzip/master/toplevel/
   # http://ruby-doc.org/stdlib-2.0.0/libdoc/tempfile/rdoc/Tempfile.html
   # http://thinkingeek.com/2013/11/15/create-temporary-zip-file-send-response-rails/
-  # TODO: modularize everything. Make CSV file temporary too.
   #
   def download
     portfolio = Portfolio.find(params[:id])
     filename = "#{Date.today}_portfolio_#{portfolio.name}".delete(' ')
-    temp_file = Tempfile.new("#{filename}.zip")
+    temp_zip = Tempfile.new("#{filename}.zip", Rails.root.join('tmp', 'zip'))
 
     begin
       # Initialize the temp file as a zip file
-      Zip::OutputStream.open(temp_file) { |zos| }
+      Zip::OutputStream.open(temp_zip) { |zos| }
 
       # Add files to zip file
-      Zip::File.open(temp_file.path, Zip::File::CREATE) do |zip|
-        # for building_type in building_types, create csv for that building type
+      Zip::File.open(temp_zip.path, Zip::File::CREATE) do |zip|
+        # For building_type in building_types, create csv for that building type
         # if the query result is not empty
         BuildingType.find_each do |building_type|
           buildings = portfolio.buildings.where(building_type: building_type).all
-          unless buildings.blank?
-            csv_name = "#{filename}_#{building_type.name}".delete(' ')
-            # populate CSV
-            CSV.open(csv_name, 'wb') do |csv|
-              # add column headers to CSV
-              csv << Building.column_names
-              # add rows to CSV
-              buildings.each do |building|
-                csv << Building.column_names.map { |attr| building.send(attr) }
-              end
-              zip.add("#{csv_name}.csv", csv.path)
-            end
-          end
+          csv_name = "#{filename}_#{building_type.name}.csv".delete(' ')
+          add_csv_to_zip(zip, csv_name, buildings) unless buildings.blank?
         end
       end
 
-      # Read in the binary data of temp_file and send to browser as attachment
-      zip_data = File.read(temp_file.path)
+      # Read in the binary data of temp_zip and send to browser as attachment
+      zip_data = File.read(temp_zip.path)
       send_data(zip_data, type: 'application/zip', filename: "#{filename}.zip")
     ensure
-      # Close and delete temp_file
-      temp_file.close
-      temp_file.unlink
+      # Close and delete temp_zip no matter what happens in the block
+      temp_zip.close
+      temp_zip.unlink
+      # Copypasta to delete CSV files in tmp/zip/ directory
+      FileUtils.rm_rf(Dir.glob(Rails.root.join('tmp', 'zip', '*')))
     end
   end
   helper_method :download
+
+  private
+
+  ##
+  # Given a zip file object, a name, and a list of buildings of a given Building
+  # Type, create a temporary CSV file with the given name. Populate the CSV with
+  # the attributes of all the buildings. Then, add the CSV to the zip.
+  # This function creates a tempfile, but it does not immediately delete the
+  # file. Instead, deletion is postponed until the main function sends the zip
+  # to the browser.
+  #
+  # References:
+  # http://ruby-doc.org/stdlib-2.0.0/libdoc/csv/rdoc/CSV.html
+  #
+  def add_csv_to_zip(zip, csv_name, buildings)
+    temp_csv = Tempfile.new(csv_name, Rails.root.join('tmp', 'zip'))
+
+    begin
+      # Create and populate CSV with given name
+      CSV.open(temp_csv, 'wb') do |csv|
+        # Add column headers to CSV
+        csv << Building.column_names
+        # Add a row of building attribute values to the CSV for each building
+        buildings.each do |building|
+          csv << Building.column_names.map { |attr| building.send(attr) }
+        end
+        zip.add(csv_name, csv.path)
+      end
+    end
+  end
 end
