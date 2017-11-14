0356c0cd8ba23ee6083e296e153bf81995632999
Kevin Li <kevintli98@gmail.com>
2017-11-13 18:43:12 -0800

    Redirect asset managers to their portfolio page

--- app/controllers/asset_managers_controller.rb
+++ app/controllers/asset_managers_controller.rb
@@ -1,2 +1,6 @@
 class AssetManagersController < ApplicationController
+  def show
+    portfolio = AssetManager.find(params[:id]).portfolio
+    redirect_to "/portfolios/#{portfolio.id}"
+  end
 end
