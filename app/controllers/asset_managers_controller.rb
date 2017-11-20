244b1af53fca387f37741e0de9294ef7f69d537f
arpanpal <arpan.pal1234@gmail.com>
2017-11-19 17:32:21 -0800

    user serializers updated

--- app/controllers/asset_managers_controller.rb
+++
@@ -1,6 +0,0 @@
-class AssetManagersController < ApplicationController
-  def show
-    portfolio = AssetManager.find(params[:id]).portfolio
-    redirect_to "/portfolios/#{portfolio.id}"
-  end
-end
