e57c97d52868dad4fe1978d6bd7bb54c45e968d0
Kevin Li <kevintli98@gmail.com>
2017-11-28 20:51:26 -0800

    Set up Sass

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-module.exports = environment
+const merge = require('webpack-merge');
+
+const myCssLoaderOptions = {
+  modules: true,
+  sourceMap: true,
+  localIdentName: '[name]__[local]___[hash:base64:5]'
+};
+
+const CSSLoader = environment.loaders.get('style').use.find(el => el.loader === 'css-loader');
+
+CSSLoader.options = merge(CSSLoader.options, myCssLoaderOptions);
+
+module.exports = environment;
