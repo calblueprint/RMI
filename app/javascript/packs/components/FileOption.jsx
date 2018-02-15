622c9119e50cad8f13167ec41296c579fccf389a
Tony Yang <tonyyanga@gmail.com>
2018-02-14 19:30:40 -0800

    add frontend code to access uploaded file

--- app/javascript/packs/components/FileOption.jsx
+++ app/javascript/packs/components/FileOption.jsx
@@ -2,16 +2,37 @@ import React from 'react';
 
 class FileOption extends React.Component {
 
-  componentDidMount() {
-    // TODO: Should it be deleted?
+  // TODO: handle click of upload button
+  handleUpload(e) {
+
+  }
+
+  // get link to file already uploaded
+  getFileLink() {
+    if (this.props.answer) {
+      // Need to generate link
+      return "/api/answers/" + this.props.answer.id;
+    } else {
+      return "";
+    }
   }
 
   render() {
+    const currentFileName = this.props.answer ? this.props.answer.attachment_file_name : "";
+    const currentFileLink = this.getFileLink()
+
+    // filler is None when no file corresponds to this field
+    const filler = this.props.answer ? "" : "None";
+
     return (<div>
+      File uploaded: <a href={currentFileLink}>{currentFileName}</a>{filler}<br>
       <input
         type="file"
-        onChange={this.onChange}
       />
+      <button
+        type="submit" value="Upload"
+        onclick={this.handleUpload(e)}
+      >Upload</button>
     </div>)
   }
 }
