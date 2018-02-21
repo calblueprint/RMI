9b279f2ec5570f9ed8c83844e168fd0ef0dff07a
Kevin Li <kevintli98@gmail.com>
2018-02-21 12:28:30 -0800

    Fix indentation (#140)
    * Fix indentations
    
    * Fix indentations

--- app/javascript/packs/containers/BuildingListContainer.jsx
+++ app/javascript/packs/containers/BuildingListContainer.jsx
@@ -7,33 +7,33 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class BuildingListContainer extends React.Component {
-    render() {
-        const buildings = this.props.buildings;
-        return (<div>
-            <h2>Buildings</h2>
-            <hr />
-            {Object.keys(buildings).map(id => {
-                return (<p key={id}>{this.props.buildings[id].name} |
-                    <Link to={`/buildings/${id}`}>Details</Link>
-                </p>)
-            })}
-        </div>);
-    }
+  render() {
+    const buildings = this.props.buildings;
+    return (<div>
+      <h2>Buildings</h2>
+      <hr />
+      {Object.keys(buildings).map(id => {
+        return (<p key={id}>{this.props.buildings[id].name} |
+          <Link to={`/buildings/${id}`}>Details</Link>
+        </p>)
+      })}
+    </div>);
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        buildings: getBuildings(state)
-    };
+  return {
+    buildings: getBuildings(state)
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        buildingActions: bindActionCreators(BuildingActions, dispatch),
-    };
+  return {
+    buildingActions: bindActionCreators(BuildingActions, dispatch),
+  };
 }
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(BuildingListContainer);
