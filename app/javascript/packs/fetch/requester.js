31c464f79db7320a2db4310765d3883636155990
Kevin Li <kevintli98@gmail.com>
2018-03-04 19:22:16 -0800

    Add get and destroy methods to requester

--- app/javascript/packs/fetch/requester.js
+++ app/javascript/packs/fetch/requester.js
@@ -1,19 +1,27 @@
+export async function get(route) {
+  return doFetchRequest(route, 'GET');
+}
+
+export async function destroy(route) {
+  return doFetchRequest(route, 'DELETE');
+}
+
 export async function post(route, body) {
-  return fetchRequest(route, 'POST', body);
+  return doFetchRequest(route, 'POST', body);
 }
 
 export async function patch(route, body) {
-  return fetchRequest(route, 'PATCH', body);
+  return doFetchRequest(route, 'PATCH', body);
 }
 
-async function fetchRequest(route, method, body) {
+async function doFetchRequest(route, method, body) {
   const bodyData = JSON.stringify(body);
   return await fetch(route, {
     method: method,
     body: bodyData,
     headers: {
-      "X-CSRF-Token": document.querySelector("meta[name='csrf-token']").content,
-      "Content-Type": "application/json"
+      'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content,
+      'Content-Type': 'application/json'
     },
     credentials: 'same-origin'
   }).then(resp => resp.json());
