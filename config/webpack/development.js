96eff038b188ff2ed65a4b9d1e0078e77a33d0f4
Ethan Lee <hello@ethanl.ee>
2018-03-09 18:45:41 -0800

    Webpack config cleanup

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -4,18 +4,11 @@ const FriendlyErrorsWebpack = require('friendly-errors-webpack-plugin');
 
 const config = environment.toWebpackConfig();
 config.devtool = 'cheap-module-eval-source-map';
-// config.devServer.hot = true;
+
 config.devServer.quiet = true;
 config.devServer.overlay = true;
 
-// config.plugins.push(new webpack.NamedModulesPlugin());
-// config.plugins.push(new webpack.HotModuleReplacementPlugin());
 config.plugins.push(new FriendlyErrorsWebpack());
-
-// config.module.rules.push({
-//   test: /\.(s*)css$/,
-//   use: ["style-loader", "css-loader", "sass-loader"]
-// });
 
 config.output.filename =  '[name].bundle.js';
 
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,14 +2,4 @@ const { environment } = require('@rails/webpacker');
 
 const merge = require('webpack-merge');
 
-// const myCssLoaderOptions = {
-//   modules: true,
-//   sourceMap: true,
-//   localIdentName: '[name]__[local]___[hash:base64:5]'
-// };
-
-// const CSSLoader = environment.loaders.get('style').use.find(el => el.loader === 'css-loader');
-
-// CSSLoader.options = merge(CSSLoader.options, myCssLoaderOptions);
-
 module.exports = environment;
