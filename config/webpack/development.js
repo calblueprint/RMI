b2a4d2e5d2e43116a72d743a41af8c50280425b9
Ethan Lee <hello@ethanl.ee>
2018-03-09 18:36:52 -0800

    Prepare development environment for CSS: HMR and Sass imports enabled, better Webpack output and overlay in-browser

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,6 +1,22 @@
-const environment = require('./environment')
+const environment = require('./environment');
+const webpack = require('webpack');
+const FriendlyErrorsWebpack = require('friendly-errors-webpack-plugin');
 
-const config = environment.toWebpackConfig()
-config.devtool = 'sourcemap';
+const config = environment.toWebpackConfig();
+config.devtool = 'cheap-module-eval-source-map';
+// config.devServer.hot = true;
+config.devServer.quiet = true;
+config.devServer.overlay = true;
 
-module.exports = config
+// config.plugins.push(new webpack.NamedModulesPlugin());
+// config.plugins.push(new webpack.HotModuleReplacementPlugin());
+config.plugins.push(new FriendlyErrorsWebpack());
+
+// config.module.rules.push({
+//   test: /\.(s*)css$/,
+//   use: ["style-loader", "css-loader", "sass-loader"]
+// });
+
+config.output.filename =  '[name].bundle.js';
+
+module.exports = config;
