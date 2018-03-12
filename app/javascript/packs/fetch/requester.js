632fa1d261f6c8673132bbc7dd1181c432f95a75
Kevin Li <kevintli98@gmail.com>
2018-03-12 02:24:52 -0700

    Add inline documentation on catching requester errors

--- app/javascript/packs/fetch/requester.js
+++ app/javascript/packs/fetch/requester.js
@@ -1,3 +1,12 @@
+/**
+ * Contains async functions for standard HTTP requests.
+ *
+ * IMPORTANT:
+ * Each requester function returns a promise that resolves to the JSON response if successful.
+ * However, it will throw an error if a non-200 response is received.
+ * Any code that uses requester functions needs to catch and handle this case explicitly.
+ */
+
 export async function get(route) {
   return doFetchRequest(route, 'GET');
 }
@@ -14,6 +23,14 @@ export async function patch(route, body) {
   return doFetchRequest(route, 'PATCH', body);
 }
 
+/**
+ *
+ * @param route         Route for the request (e.g. /answers/11)
+ * @param method        Method to use ('GET', 'POST', etc.)
+ * @param body          Request body. Can be left undefined, for example for GET requests.
+ *
+ * @returns {Promise.<*>}   A promise that resolves to the JSON response if the request was successful.
+ */
 async function doFetchRequest(route, method, body) {
   const bodyData = JSON.stringify(body);
   return await fetch(route, {
