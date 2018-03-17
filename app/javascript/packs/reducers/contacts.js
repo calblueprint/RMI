9dd47012440cfd08b31eadf11728ec7cd4de13ab
Tony Yang <tonyyanga@gmail.com>
2018-03-16 23:13:32 -0700

    fix reducer to support correct format in redux: contacts are unordered and has no id

--- app/javascript/packs/reducers/contacts.js
+++ app/javascript/packs/reducers/contacts.js
@@ -4,15 +4,26 @@ import {
 } from '../constants';
 
 function attachContact(state, action) {
-  const email = action.email;
-  return {
-    ...state,
-    [email]: {
+  var found = false;
+  for (var i = 0; i < state.length; i++) {
+    if (state[i].email == action.email) {
+      state[i] = {
+        email: action.email,
+        firstName: action.firstName,
+        lastName: action.lastName
+      };
+      found = true;
+      break;
+    }
+  }
+  if (!found) {
+    state.push({
       email: action.email,
       firstName: action.firstName,
       lastName: action.lastName
-    }
+    });
   }
+  return state;
 }
 
 export default function contacts(state = {}, action) {
