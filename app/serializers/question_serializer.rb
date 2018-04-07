054484358a2643cd3c035aadd83be243420aabc6
Kevin Li <kevintli98@gmail.com>
2018-04-07 16:12:23 -0700

    Add file upload question to seeds file

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -37,8 +37,6 @@ class QuestionSerializer < ActiveModel::Serializer
         object.dropdown_options
       when 'RangeOption'
         object.range_options
-      when 'file'
-        object.file_option
       else
         []
       end
