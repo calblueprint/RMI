ad9972564688622428b358cb373dceb35be33584
Ethan Lee <hello@ethanl.ee>
2018-04-06 18:52:15 -0700

    Add button input scheme for dropdown options with <= 5 options

--- app/javascript/packs/components/DropdownOption.jsx
+++ app/javascript/packs/components/DropdownOption.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import fontawesome from '@fortawesome/fontawesome';
+import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
+
 class DropdownOption extends React.Component {
   componentDidMount() {
     if (this.props.answer) {
@@ -14,17 +17,51 @@ class DropdownOption extends React.Component {
     }
   }
 
+  currentValue() {
+    return this.props.answer ? this.props.answer.selected_option_id : "unselected";
+  }
+
   onChange(optionId) {
     this.saveAnswer(optionId, this.props.options[optionId].text);
   }
 
   saveAnswer(optionId, text) {
+    if (this.currentValue() == optionId) {
+      // For unselecting an option (through the button control scheme)
+      optionId = null;
+      text = null;
+    }
     this.props.onChange(optionId, text);
     this.props.onSave(optionId, text);
   }
 
   render() {
-    const currentValue = this.props.answer ? this.props.answer.selected_option_id : "unselected";
+    const currentValue = this.currentValue();
+    const options = Object.values(this.props.options);
+    if (options.length <= 5) {
+      return options.map((option) => (
+        <button
+          key={option.id}
+          value={option.id}
+          onClick={(e) => this.handleChange(e)}
+          className={`input__dropdown ${
+            currentValue === option.id ? 'input__dropdown--selected' : ''
+          }`}
+        >
+          {option.text}
+          {
+            currentValue === option.id ?
+            <i
+              style={{ marginLeft: '10px' }}
+              dangerouslySetInnerHTML={{
+                __html: fontawesome.icon(faCheck).html[0]
+              }}
+            /> : null
+          }
+        </button>
+      ));
+    }
+
     return (<div>
       <select
         value={currentValue}
@@ -34,7 +71,7 @@ class DropdownOption extends React.Component {
         ref={(ref) => this.ref = ref}
       >
         <option value="unselected" disabled>Select an option</option>
-        {Object.values(this.props.options).map((option) => {
+        {options.map((option) => {
           return (<option value={option.id} key={option.id}>{option.text}</option>)
         })}
       </select>
