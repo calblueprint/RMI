973a96534fd74fb53d87ee08bf833cd5cda621d2
arpanpal <arpan.pal1234@gmail.com>
2018-04-08 15:10:21 -0700

    underestimate questions remaining

--- app/javascript/packs/selectors/answersSelector.js
+++ app/javascript/packs/selectors/answersSelector.js
@@ -1,3 +1,5 @@
+import {getPotentialDependentQuestions} from "./questionsSelector";
+
 export function getAnswerForQuestionAndBuilding(questionId, buildingId, state) {
   return state.buildings[buildingId].answers[questionId]
 }
@@ -8,12 +10,32 @@ export function getAnswerForQuestionAndBuilding(questionId, buildingId, state) {
 //## if no questions are provided, the building did not have any questions for the users so they have no questions
 //to answer for that building
 export function getRemainingAnswersforCategory(questions, buildingId, state) {
+  let dependentQuestions = [];
   return questions.reduce((count, question) => {
-    let answer = state.buildings[buildingId].answers[question.id];
-    if (!answer || !answer.text.trim() && !answer.attachment_file_name) {
-      return count + 1;
+    if (isUnanswered(question, buildingId, state) && !dependentQuestions.includes(question)) {
+        if (isDelegated(question, buildingId, state)) {
+          let currDQ = getPotentialDependentQuestions(question, state);
+          dependentQuestions.push(...currDQ);
+          count += currDQ.length;
+        } else {
+          return count + 1;
+        }
     }
     return count;
   }, 0);
+}
+
+export function isUnanswered(question, buildingId, state) {
+  let answer = state.buildings[buildingId].answers[question.id];
+  if (!answer || !answer.text.trim() && !answer.attachment_file_name) {
+    return true;
+  }
+}
+
+export function isDelegated(question, buildingId, state) {
+  let answer = state.buildings[buildingId].answers[question.id];
+  if (answer.delegation_email) {
+    return true;
+  }
 }
 
