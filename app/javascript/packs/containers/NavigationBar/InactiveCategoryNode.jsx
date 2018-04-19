044a6fd886b6cda8adf14772fd95dabfdaaee5f2
Kevin Li <kevintli98@gmail.com>
2018-04-18 21:21:21 -0700

    Add delegate and review mode icons to bus map

--- app/javascript/packs/containers/NavigationBar/InactiveCategoryNode.jsx
+++ app/javascript/packs/containers/NavigationBar/InactiveCategoryNode.jsx
@@ -5,24 +5,20 @@ import { Link } from 'react-router-dom';
 class InactiveCategoryNode extends React.Component {
   render() {
     return (<div className="category category--inactive">
-      <Link to={`/buildings/${this.props.buildingId}/edit/${this.props.category.id}`}>
+      <Link to={this.props.path}>
         <div className="category__circle">
-          {this.props.index + 1}
+          {this.props.label}
         </div>
       </Link>
-      <h1>{this.props.category.name}</h1>
+      <h1>{this.props.name}</h1>
     </div>);
   }
 }
 
 InactiveCategoryNode.propTypes = {
-  category: PropTypes.shape({
-    building_type_id: PropTypes.number.isRequired,
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired
-  }).isRequired,
-  index: PropTypes.number.isRequired,
-  buildingId: PropTypes.number.isRequired
+  label: PropTypes.object.isRequired,
+  path: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
 };
 
 export default InactiveCategoryNode;
