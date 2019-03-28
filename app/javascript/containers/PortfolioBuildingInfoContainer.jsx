417579377b57e4a82c2ab1fded152f39a7e93bfc
didvi <divi@berkeley.edu>
2019-03-28 16:17:05 -0600

    minor changes

---
+++ app/javascript/containers/PortfolioBuildingInfoContainer.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
+import PropTypes from 'prop-types';
+import { getStreetAddressByBuildingId, getCityStateAddressByBuildingId, getNameByBuildingId } from "../selectors/buildingsSelector";
+import { getCategoriesForBuilding } from '../selectors/categoriesSelector';
+import QuestionaireCategoryContainer from './QuestionaireCategoryContainer';
+
+class PortfolioBuildingInfoContainer extends React.Component {
+  render() {
+    return (
+      <div key={this.props.building_id}>
+          <h2 className='building_title'>{this.props.name}</h2>
+          <h3 className='building_address'>{this.props.streetAddress}<br></br>{this.props.cityStateAddress}</h3>
+          <QuestionaireCategoryContainer building_id={ this.props.building_id } categories={ this.props.categories }></QuestionaireCategoryContainer>
+      </div>
+    );
+  }
+}
+
+PortfolioBuildingInfoContainer.propTypes = {
+  building_id: PropTypes.string.isRequired,
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    name: getNameByBuildingId(ownProps.building_id, state),
+    streetAddress: getStreetAddressByBuildingId(ownProps.building_id, state),
+    cityStateAddress: getCityStateAddressByBuildingId(ownProps.building_id, state),
+    categories: getCategoriesForBuilding(ownProps.building_id, state)
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {};
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PortfolioBuildingInfoContainer);
