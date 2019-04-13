9e480ae939769a3e2f01950c3d4c0001bd7c4588
Ethan Lee <hello@ethanl.ee>
2019-04-13 14:36:14 -0700

    New editable sidebar categories for questionnaire builder

--- app/javascript/components/FAIcon.jsx
+++ app/javascript/components/FAIcon.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import fontawesome from "@fortawesome/fontawesome";
 
-function FAIcon({ iconObj, style }) {
+function FAIcon({ iconObj, style, ...props }) {
   return (
     <i
+      {...props}
       style={{ fontSize: "14pt", ...style }}
       dangerouslySetInnerHTML={{
         __html: fontawesome.icon(iconObj).html[0]
