feb849618ee869091e9730c53ee9abb37bb0a76f
Kevin Deguzman <deguzmankevin@hotmail.com>
2019-04-18 22:42:12 -0700

    import from old PR

--- app/javascript/containers/ReviewModeContainer.jsx
+++ app/javascript/containers/ReviewModeContainer.jsx
@@ -3,33 +3,22 @@
  * ROUTE - /buildings/:bId/review
  */
 
-import React from 'react';
+import React from "react";
 
-import QuestionContainer from './QuestionContainer';
+import QuestionContainer from "./QuestionContainer";
 
-import {getAnswerForQuestionAndBuilding} from '../selectors/answersSelector';
-import {getPotentialDependentQuestions} from "../selectors/questionsSelector";
-import {connect} from 'react-redux';
-import {getQuestionsByBuilding} from '../selectors/questionsSelector';
-import {getQuestionsByCategory} from "../utils/QuestionsFilter";
-import {getCategoriesForBuilding} from "../selectors/categoriesSelector";
-import CategoryHeader from '../components/CategoryHeader';
+import { getAnswerForQuestionAndBuilding } from "../selectors/answersSelector";
+import { getPotentialDependentQuestions } from "../selectors/questionsSelector";
+import { connect } from "react-redux";
+import { getQuestionsByBuilding } from "../selectors/questionsSelector";
+import { getQuestionsByCategory } from "../utils/QuestionsFilter";
+import { getCategoriesForBuilding } from "../selectors/categoriesSelector";
+import CategoryHeader from "../components/CategoryHeader";
 
-import {post, patch} from '../fetch/requester';
-
-async function postDelegations(delegations) {
-  var body = {delegations};
-
-  try {
-    let resp = await post('/api/delegations', body);
-    return true;
-  } catch (error) {
-    return false;
-  }
-}
+import { post, patch } from "../fetch/requester";
 
 function mapCategorytoQuestions(categoryMap, categoryId, building) {
-  return categoryMap[categoryId].map((question) => {
+  return categoryMap[categoryId].map(question => {
     // Only display non-dependent questions initially
     if (question.parent_option_id) {
       //TODO: look at this later
@@ -50,55 +39,69 @@ function mapCategorytoQuestions(categoryMap, categoryId, building) {
         {...question}
       />
     );
-  })
+  });
 }
 
 class ReviewModeContainer extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      status_string: "Delegations have not been saved yet.",
-    }
+      status_string: "Delegations have not been saved yet."
+    };
   }
 
   // called when delegation should be submitted
   // should synchronously submit delegations since user expects success
   async submitDelegation() {
-    var parentQuestionsForDelegations = this.props.questions.filter((question) => {
-      answer = this.props.getAnswer(question.id);
-      return answer && !answer.text && answer.delegation_email;
-    });
+    let delegations = this.getDelegations();
+    if (delegations.length == 0) {
+      this.setState({
+        status_string: "There were no delegations to be saved!"
+      });
+    } else {
+      this.setState({ status_string: "Saving delegations!" });
+      try {
+        let response = await post("/api/delegations", { delegations });
+        this.setState({ status_string: "Delegations saved." });
+      } catch (error) {
+        this.setState({
+          status_string: "Saving delegations failed. Try again?"
+        });
+      }
+    }
+  }
+
+  getDelegations() {
+    var parentQuestionsForDelegations = this.props.questions.filter(
+      question => {
+        answer = this.props.getAnswer(question.id);
+        return answer && !answer.text && answer.delegation_email;
+      }
+    );
 
     var delegations = [];
     for (var i = 0; i < parentQuestionsForDelegations.length; i++) {
       var question = parentQuestionsForDelegations[i];
       var answer = this.props.getAnswer(question.id);
-      var allDependentQuestions = this.props.getPotentialDependentQuestions(question);
+      var allDependentQuestions = this.props.getPotentialDependentQuestions(
+        question
+      );
       allDependentQuestions.push(question);
 
-      allDependentQuestions.map((currentQuestion) => {
+      allDependentQuestions.map(currentQuestion => {
         var currentAnswer = this.props.getAnswer(currentQuestion.id);
         if (currentAnswer) {
           var delegation = {
             email: answer.delegation_email,
             first_name: answer.delegation_first_name,
             last_name: answer.delegation_last_name,
-            answer_id: currentAnswer.id,
+            answer_id: currentAnswer.id
           };
           delegations.push(delegation);
         }
-      })
-      ;
+      });
     }
-
-    this.setState({status_string: "Saving delegations!"});
-    var success = await postDelegations(delegations);
-    if (success) {
-      this.setState({status_string: "Delegations saved."});
-    } else {
-      this.setState({status_string: "Saving delegations failed. Try again?"});
-    }
+    return delegations;
   }
 
   populateQuestionStack(building, questions) {
@@ -108,16 +111,21 @@ class ReviewModeContainer extends React.Component {
     for (let category in this.props.categories) {
       let stateCategory = this.props.categories[category];
       count += 1;
-      categoryMap[stateCategory.id] = getQuestionsByCategory(stateCategory.id, questions);
-      stack.push(<CategoryHeader
-        category={stateCategory}
-        number={count}
-        buildingId={building.id}
-      />);
+      categoryMap[stateCategory.id] = getQuestionsByCategory(
+        stateCategory.id,
+        questions
+      );
+      stack.push(
+        <CategoryHeader
+          category={stateCategory}
+          number={count}
+          buildingId={building.id}
+        />
+      );
       stack = stack.concat(
         <table cellSpacing="0">
           <tbody>
-          {mapCategorytoQuestions(categoryMap, stateCategory.id, building)}
+            {mapCategorytoQuestions(categoryMap, stateCategory.id, building)}
           </tbody>
         </table>
       );
@@ -125,17 +133,18 @@ class ReviewModeContainer extends React.Component {
     return stack;
   }
 
-
   render() {
     return (
       <div>
         {this.populateQuestionStack(this.props.building, this.props.questions)}
         <div className="delegation">
-          <button type="submit"
-                  value="Submit Form"
-                  onClick={(e) => this.submitDelegation()}
-                  className="next-button next-button--submit"
-          >Submit Form
+          <button
+            type="submit"
+            value="Submit Form"
+            onClick={e => this.submitDelegation()}
+            className="next-button next-button--submit"
+          >
+            Submit Form
           </button>
           <p>{this.state.status_string}</p>
         </div>
@@ -144,19 +153,20 @@ class ReviewModeContainer extends React.Component {
   }
 }
 
-
 function mapStateToProps(state, ownProps) {
   return {
-    getPotentialDependentQuestions: (question) => getPotentialDependentQuestions(question, state),
+    getPotentialDependentQuestions: question =>
+      getPotentialDependentQuestions(question, state),
     building: state.buildings[ownProps.building.id],
     questions: getQuestionsByBuilding(ownProps.building.id, state),
-    getAnswer: (questionId) => getAnswerForQuestionAndBuilding(questionId, ownProps.building.id, state),
-    categories: getCategoriesForBuilding(ownProps.building.id, state),
+    getAnswer: questionId =>
+      getAnswerForQuestionAndBuilding(questionId, ownProps.building.id, state),
+    categories: getCategoriesForBuilding(ownProps.building.id, state)
   };
 }
 
 function mapDispatchToProps(dispatch) {
-  return {}
+  return {};
 }
 
 export default connect(
