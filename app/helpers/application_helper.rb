ea2090495d9ef4025d024ae8c19e15633b98e9ff
Kevin Deguzman <deguzmankevin@hotmail.com>
2019-04-24 15:15:26 -0700

    successful notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -103,5 +103,16 @@ module ApplicationHelper
       current_asset_manager
     end
   end
+
+  def custom_bootstrap_flash
+    flash_messages = []
+    flash.each do |type, message|
+      type = 'success' if type == 'notice'
+      type = 'error'   if type == 'alert'
+      text = "<script>toastr.#{type}('#{message}');</script>"
+      flash_messages << text.html_safe if message
+    end
+    flash_messages.join("\n").html_safe
+  end
   # rubocop:enable AlignHash
 end
